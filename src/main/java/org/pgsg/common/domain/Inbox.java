56c7743a5d80eba51de4650102c095a0f0f287f9
jin <yjj1544@gmail.com>
2026-05-13 13:55:25 +0900

    fix: Inbox.java status에 @Enumerated 추가

--- src/main/java/org/pgsg/common/domain/Inbox.java
+++ src/main/java/org/pgsg/common/domain/Inbox.java
@@ -13,6 +13,8 @@ import jakarta.persistence.AccessType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
@@ -43,6 +45,7 @@ public class Inbox {
 	protected String messageGroup;	//co
 
 	@Builder.Default
+	@Enumerated(EnumType.STRING)
 	@Column
 	protected InboxStatus status=InboxStatus.RECEIVED;
 
